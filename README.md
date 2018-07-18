b2cc8a307fbe65f98653d32b42a7eb3063b9576a
klaymon <kyle.laymon@gmail.com>
2018-07-17 22:33:30 -0700

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+## Getting Started
+- Install Node https://nodejs.org/en/download/
+- Install npm https://docs.npmjs.com/getting-started/installing-node#installing-npm-from-the-nodejs-site
+- Sign up for IFTTT https://ifttt.com/
+- Create a new IFTTT Applet https://ifttt.com/create
+  - Use Webhooks as the "if this" trigger
+  - Save your Webhooks Event Name! It will be saved in .env as EVENT_NAME
+  - After setting up the Applet go to https://ifttt.com/services/maker_webhooks/settings and get the URL token (end of the url, starts with fn)
+- Get your Twitch oauth from https://twitchapps.com/tmi/
+
+Fill out your EVENT_NAME, IFTTT_KEY (starts with `fn-`), your TWITCH_NAME and TWITCH_CODE (starts with `oauth:`) in the .env file
+
+## To run:
+```
+npm install
+npm run start
+```
---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+const request = require('request-promise')
+const TwitchJS = require('twitch-js')
+const { EVENT_NAME, IFTTT_KEY, TWITCH_CODE, TWITCH_NAME } = process.env
+
+let Bot
+const monitoredChannels = [] // array of string channel names (each needs to start with a # eg #ninja)
+const monitoredTerms = [TWITCH_NAME] // or any additional terms you care about
+const opts = {
+  identity: {
+    username: TWITCH_NAME,
+    password: TWITCH_CODE
+  },
+  channels: [ ], // array of string channel names to join on connect (each WITHOUT a # eg ninja)
+  reconnect: true,
+  maxReconnectAttempts: 5
+}
+
+function onChatHandler (channel, userstate = {}, message, self) {
+  if (userstate.username === TWITCH_NAME) return
+
+  if (isInMonitoredChannel(channel) || includesMonitoredTerm(message)) {
+    const totalMessage = `${channel}:\t${userstate.username}: ${message}\n`
+    return sendMessage(totalMessage)
+  }
+}
+
+function runBot () {
+  Bot = new TwitchJS.client(opts)
+
+  Bot.on('connected', onConnectedHandler)
+  Bot.on('disconnected', onDisconnectedHandler)
+  Bot.on('chat', onChatHandler)
+
+  Bot.connect()
+}
+
+function onConnectedHandler (addr, port) {
+  console.info(`Connected to Twitch ${addr}:${port}`)
+}
+
+function onDisconnectedHandler (reason) {
+  console.info(`Disconnected. Reason: ${reason}`)
+  process.exit(1)
+}
+
+function sendMessage (message) {
+  try {
+    const url = `https://maker.ifttt.com/trigger/${EVENT_NAME}/with/key/${IFTTT_KEY}`
+    return request(url, {
+      body: { value1: message },
+      json: true,
+      method: 'POST'
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+module.exports = runBot()
+
+function isInMonitoredChannel (channel) {
+  return monitoredChannels.includes(channel)
+}
+
+function includesMonitoredTerm (message) {
+  return monitoredTerms.some(function (term) {
+    return message.includes(term)
+  })
+}
